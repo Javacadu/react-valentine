0bfa14a6dfa2687ab7e13e096d3b3c24c68cc377
Jacob Atanacio <jacobatanacio@gmail.com>
2024-02-13 19:16:00 -0800

    Fully customized valentine proposal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import Jikki from './Jikki.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 +16;
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  };
+
+  const getNoButtonText = () => {
+    const phrases = [
+      "No",
+      "Are you sure?",
+      "Really sure?",
+      "Think again!",
+      "Last chance!",
+      "Surely not?",
+      "You might regret this!",
+      "Give it another thought!",
+      "Are you absolutely certain?",
+      "This could be a mistake!",
+      "Have a heart!",
+      "Don't be so cold!",
+      "Change of heart?",
+      "Wouldn't you reconsider?",
+      "Is that your final answer?",
+      "You're breaking my heart ;(",
+    ]
+
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        {yesPressed ? (
+          <>
+            <img src='https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif' alt='Bear Kiss' />
+            <div> Okie yey hehe </div>
+          </>
+        ) : (
+          <>
+            <img src={Jikki} className='valentines-pic' alt='Jikki' />
+
+            <p>
+              Will you be my Valentine?
+
+              <br></br>
+
+              <button className='yes-button' style={{ fontSize: yesButtonSize }} onClick={() => setYesPressed(true)}>
+                Yes
+              </button>
+
+              <button onClick={handleNoClick} className='no-button'>
+                {noCount === 0 ? 'No' : getNoButtonText()}
+              </button>
+            </p>
+          </>
+        )}
       </header>
     </div>
   );
 }
 
 export default App;
+
